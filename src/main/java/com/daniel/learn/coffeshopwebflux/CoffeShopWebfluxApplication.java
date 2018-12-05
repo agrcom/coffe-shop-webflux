9528aa40f6463f1b78b56d3ac70cf612f67a758a
Dąbrowski <dldi@gft.com>
2018-12-05 14:25:45 +0100

    Initial commit

---
+++ src/main/java/com/daniel/learn/coffeshopwebflux/CoffeShopWebfluxApplication.java
@@ -0,0 +1,12 @@
+package com.daniel.learn.coffeshopwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeShopWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeShopWebfluxApplication.class, args);
+	}
+}
